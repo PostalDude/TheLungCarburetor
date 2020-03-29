28ec799395b41f7b82dfd037429c83daba02feb1
Martin Leroux <mleroux@kinova.ca>
2020-03-29 14:17:00 -0400

    Added pressure profile class

---
+++ tlc/PressureProfile.cpp
@@ -0,0 +1,143 @@
+#include "PressureProfile.h"
+#include <iostream>
+
+
+PressureProfile::PressureProfile(float time, float ratio, float pMax, float pMin)
+{
+    m_bIsValid = true;
+    SetCycleTime(time);
+    SetInhaleRatio(ratio);
+    SetMinimumPressure(pMin);
+    SetMaximumPressure(pMax);
+    CalcTimeStamps();
+    CalcSlopes();
+}
+
+PressureProfile::PressureProfile(const float time, const float partInhale, const float partExhale, const float pMax, const float pMin)
+{
+    m_bIsValid = true;
+    SetCycleTime(time);
+    SetInhaleExhaleProporitions(partInhale, partExhale);
+    SetMinimumPressure(pMin);
+    SetMaximumPressure(pMax);
+    CalcTimeStamps();
+    CalcSlopes();
+}
+
+void PressureProfile::SetCycleTime(const float time)
+{
+    if (time >0)
+    {
+        m_fCycleTime = time;
+        
+    }
+    else
+    {
+        std::cout << "Cycle time must be larger than 0" << std::endl;
+        m_bIsValid = false;
+    }
+    
+}
+
+void PressureProfile::SetInhaleRatio(const float ratio)
+{
+    if ((ratio>0) && (ratio<1))
+    {
+        m_fInhaleRatio = ratio;
+    }
+    else
+    {
+        std::cout << "Inhale ratio must be strictly between 0 and 1" << std::endl;
+        m_bIsValid = false; 
+    }
+    
+    
+}
+
+void PressureProfile::SetInhaleExhaleProporitions(const float inhalePart, const float exhalePart)
+{
+    if ((inhalePart>0) && (exhalePart>0))
+    {
+        float total = inhalePart + exhalePart;
+        m_fInhaleRatio = inhalePart/total;
+    }
+    else
+    {
+        m_bIsValid = false;
+    }
+    
+}
+
+void PressureProfile::SetMinimumPressure(const float pMin)
+{
+    if (pMin >0)
+    {
+        m_fPmin = pMin;
+    }
+    else
+    {
+       std::cout << "Minimum pressure must be strictly positive" << std::endl;
+       m_bIsValid = false;
+    }
+    
+}
+
+void PressureProfile::SetMaximumPressure(const float pMax)
+{
+    if (pMax > m_fPmin)
+    {
+        m_fPmin = pMax;
+    }
+    else
+    {
+        std::cout << "Maximum pressure must be larger than minimum pressure" << std::endl;
+        m_bIsValid = false; 
+    }
+      
+}
+
+void PressureProfile::CalcTimeStamps()
+{
+    m_fTimeStamps[3] = m_fCycleTime;
+    m_fTimeStamps[1] = m_fInhaleRatio*m_fCycleTime;
+    m_fTimeStamps[0] = m_fTimeStamps[1]/2;
+    m_fTimeStamps[2] = m_fTimeStamps[1] + (m_fTimeStamps[3] - m_fTimeStamps[1])/2;
+}
+
+void PressureProfile::CalcSlopes()
+{
+    m_fPositiveSlope = (m_fPmax - m_fPmin)/m_fTimeStamps[0];
+    m_fNegativeSlope = (m_fPmax - m_fPmin)/(m_fTimeStamps[2] - m_fTimeStamps[1]);
+    //TODO: Validate values vs hardware limits
+}
+
+const float PressureProfile::EvalProfile(const float t)
+{
+    if (t<0)
+    {
+        float newTime = fmod(t, m_fCycleTime) + m_fCycleTime;
+        return EvalProfile(newTime);
+    }
+    else if((t>=0) && (t< m_fTimeStamps[0]))
+    {
+        return m_fPmin + m_fPositiveSlope*t;
+    }
+    else if (( t>= m_fTimeStamps[0]) && (t < m_fTimeStamps[1]))
+    {
+        return m_fPmax;
+    }
+    else if ((t >= m_fTimeStamps[1]) && (t < m_fTimeStamps[2]))
+    {
+        return m_fPmax - m_fPositiveSlope*(t - m_fTimeStamps[1]);
+    }
+    else if ((t >= m_fTimeStamps[2]) && (t < m_fTimeStamps[3]))
+    {
+        return m_fPmin;
+    }
+    else
+    {
+        float newTime = fmod(t, m_fCycleTime);
+        return EvalProfile(newTime);
+    }
+    
+}
---
+++ tlc/PressureProfile.h
@@ -0,0 +1,47 @@
+#ifndef PRESSUREPROFILE_H
+#define PRESSUREPROFILE_H
+
+
+
+class PressureProfile
+{
+    /*PressureProfiles computes a trapezoidal profile of a given amplitude and duration. Profile is defined with inhaling first*/
+public:
+    PressureProfile(const float time, const float ratio, const float pMax, const float pMin); //ratio = timeExhale / totalCyleTime
+    PressureProfile( const float time, const float partInhale, const float partExhale, const float pMax, const float pMin); // if ratio is expressed as partInhale:partExhale
+
+    //Getters
+    const float GetCycleTime()        {return m_fCycleTime;};
+    const float GetInhaleRatio()      {return m_fInhaleRatio;};
+    const float GetMaximumPressure()  {return m_fPmax;};
+    const float GetMinimumPressure()  {return m_fPmin;};
+    const bool  IsValid()             {return m_bIsValid};
+
+    //Setters
+    void SetCycleTime( const float t);
+    void SetInhaleRatio( const float ratio);
+    void SetInhaleExhaleProporitions( const float inhalePart, float exhalePart);
+    void SetMaximumPressure( const float pMax);
+    void SetMinimumPressure( const float pMin);
+
+    const float EvalProfile( const float t); //Evaluate pressure at instant t
+    void CalcTimeStamps(); //Recompute after setting cycle time or ratio
+    void CalcSlopes(); //Recompute after setting  any value
+protected:
+    //Utils
+    float Parts2Ratio(float part1, float part2);
+
+
+
+protected:
+    float m_fCycleTime;
+    float m_fInhaleRatio;
+    float m_fPmax;
+    float m_fPmin;
+    float m_fPositiveSlope;
+    float m_fNegativeSlope;
+    float m_fTimeStamps[4];
+    bool  m_bIsValid; 
+}
+
+#endif //PRESSUREPROFILE_H
